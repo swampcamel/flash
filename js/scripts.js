688769c4e6e67eb9586e8a15df5dec2737110c4b
Daniel Mace and Troy Empey <daniel@>
2018-08-07 10:58:05 -0700

    will include added content and scripts applied to click js

---
+++ js/scripts.js
@@ -0,0 +1,59 @@
+$(document).ready(function() {
+  $("#hjs").click(function() {
+    $("#js").toggle();
+  });
+  $("#hoperators").click(function() {
+    $("#operators").toggle();
+  });
+  $("#hvariables").click(function() {
+    $("#variables").toggle();
+  });
+  $("#hconventions").click(function() {
+    $("#conventions").toggle();
+  });
+  $("#hfunctions").click(function() {
+    $("#functions").toggle();
+  });
+  $("#hmethods").click(function() {
+    $("#methods").toggle();
+  });
+  $("#harguments").click(function() {
+    $("#arguments").toggle();
+  });
+  $("#hparameters").click(function() {
+    $("#parameters").toggle();
+  });
+  $("#hreturn").click(function() {
+    $("#return").toggle();
+  });
+  $("#hchaining").click(function() {
+    $("#chaining").toggle();
+  });
+  $("#hstrings").click(function() {
+    $("#strings").toggle();
+  });
+  $("#hbooleans").click(function() {
+    $("#booleans").toggle();
+  });
+  $("#hundefined").click(function() {
+    $("#undefined").toggle();
+  });
+  $("#hnan").click(function() {
+    $("#nan").toggle();
+  });
+  $("#hescape").click(function() {
+    $("#escape").toggle();
+  });
+  $("#halert").click(function() {
+    $("#alert").toggle();
+  });
+  $("#hcomments").click(function() {
+    $("#comments").toggle();
+  });
+  $("#hjquery").click(function() {
+    $("#jquery").toggle();
+  });
+  $("#hattributes").click(function() {
+    $("#attributes").toggle();
+  });
+});
